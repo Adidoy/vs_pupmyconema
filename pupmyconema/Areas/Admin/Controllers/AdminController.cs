b7478c3e252f10dd3c03264329dcf529d9ee6209
Adi Ablir <abliradrian@gmail.com>
2019-05-04 10:15:23 +0800

    AdminLTE integration to Admin Layout

---
+++ pupmyconema/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+
+namespace pupmyconema.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
